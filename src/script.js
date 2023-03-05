3fbf9b09a0b7f2d5e0ab5dcca1810b0577f7bb89
Renata <110370964+An-Renata@users.noreply.github.com>
2023-03-05 15:31:00 +0200

    Javascript functionality
    Implemented city data(name, current date and current time).
    Need to fix a bug with time update after select value from dropdown list

--- src/script.js
+++ src/script.js
@@ -3,7 +3,11 @@
 const cityName = document.querySelector(".city-name");
 const date = document.querySelector(".date");
 const citiesContainer = document.querySelector(".container");
-const mainCities = [
+const selectCityElement = document.querySelector("#cities");
+
+let updateInterval;
+
+let mainCities = [
   "Europe/Paris",
   "America/New_York",
   "Europe/London",
@@ -14,14 +18,17 @@ const formatDate = function (city) {
   const now = moment().tz(city).format("MMMM Do, YYYY");
   return now;
 };
+
 const formatTime = function (city) {
-  const currTime = moment().tz(city).format("HH:mm:ss [<small>]A[</small>]");
+  const now = moment.tz(city);
+  const currTime = now.format("HH:mm:ss [<small>]A[</small>]");
   return currTime;
 };
 
 const formatCityName = function (city) {
-  const name = moment().tz(city).tz();
-  const showCityName = name.split("/").slice(1).join(" ");
+  const name = moment().tz(city);
+  const nameExtracted = name.tz();
+  const showCityName = nameExtracted.split("/").slice(-1).join(" ");
 
   if (showCityName.includes("_")) {
     return showCityName.split("_").join(" ");
@@ -29,10 +36,38 @@ const formatCityName = function (city) {
   return showCityName;
 };
 
+function displaySelectInputs() {
+  const cityNames = moment.tz.names();
+
+  cityNames.forEach((city) => {
+    const updatedName = city
+      .split("/")
+      .slice(-1)
+      .join(" ")
+      .replaceAll("_", " ");
+    if (updatedName === "") return;
+
+    const html = `
+        <option value="${city}">${updatedName}</option>
+    `;
+    selectCityElement.insertAdjacentHTML("beforeend", html);
+  });
+}
+displaySelectInputs();
+
+const updateTime = function (cityName) {
+  const cityTimeEl = document.querySelectorAll(".current-time");
+  cityTimeEl.forEach((el, i) => {
+    const city = cityName[i];
+    const currentTime = formatTime(city);
+    el.innerHTML = currentTime;
+  });
+};
+
 const renderCityTimeData = function (cityName) {
   cityName.forEach(function (city) {
     const html = `
-            <div class="city-container">
+    <div class="city-container">
             <div class="city">
             <h3 class="city-name">${formatCityName(city)}</h3>
             <p class="date">${formatDate(city)}</p>
@@ -46,16 +81,37 @@ const renderCityTimeData = function (cityName) {
             `;
     citiesContainer.insertAdjacentHTML("beforeend", html);
   });
+  updateInterval = setInterval(updateTime, 1000, mainCities);
 };
 
 renderCityTimeData(mainCities);
 
-const updateTime = function () {
-  const cityTimeEl = document.querySelectorAll(".current-time");
-  cityTimeEl.forEach((el, i) => {
-    const city = mainCities[i];
-    const currentTime = formatTime(city);
-    el.innerHTML = currentTime;
+const updateSelectElement = function (e) {
+  const city = e.target.value;
+  const cityRowContainer = document.querySelectorAll(".city-container");
+  console.log(city);
+
+  if (!city) return;
+
+  cityRowContainer.forEach((el) => {
+    if (city) el.style.display = "none";
   });
+  const html = `
+    <div class="city-container">
+    <div class="city">
+    <h3 class="city-name">${formatCityName(city)}</h3>
+    <p class="date">${formatDate(city)}</p>
+    </div>
+    <div class="city-time">
+    <p class="current-time">
+    ${formatTime(city)}
+    </p>
+    </div>
+   </div>`;
+
+  clearInterval(updateInterval);
+
+  updateInterval = setInterval(updateTime, 1000, [city]);
+  citiesContainer.insertAdjacentHTML("beforeend", html);
 };
-setInterval(updateTime, 1000);
+selectCityElement.addEventListener("change", updateSelectElement);
